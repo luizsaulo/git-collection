e8f8211a9c342329db4b1ebc28c2e7c1cbb1f44c
Luiz <luizsaulo@yahoo.com.br>
2022-03-25 14:59:47 -0300

    implementation of lazy and react suspense to make the page load faster

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -15,7 +15,7 @@ interface GithubRepository {
   }
 }
 
-export const Dashboard: React.FC = () => {
+  const Dashboard: React.FC = () => {
   const [repos, setRepos] = React.useState<GithubRepository[]>(() => {
     const storageRepos = localStorage.getItem('@GitCollection:repositories');
 
@@ -102,3 +102,4 @@ export const Dashboard: React.FC = () => {
   )
 };
 
+export default Dashboard;
